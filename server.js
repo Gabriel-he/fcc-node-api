66cccd6d716bc4ba8b3cce97f7863478de9b49c3
Gabriel <gabriel.hetao@gmail.com>
2017-05-10 02:19:31 +0000

    timestamp

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:dateString', function (req, res) {
+  console.log(req.params.dateString);
+  var dateString = req.params.dateString;
+  var data;
+  var date;
+  if (dateString == '' || dateString == null) {
+      data = {
+        unix: null,
+        natural: null,
+      }
+  } else if (isNaN(parseInt(dateString))) {
+      date =  new Date(dateString);
+      data = {
+        unix: date.getTime(),
+        natural: date.toLocaleDateString(),
+      }
+  } else if (typeof(parseInt(dateString)) == 'number') {
+      date =  new Date(parseInt(dateString));
+      data = {
+        unix: date.getTime(),
+        natural: date.toLocaleDateString(),
+      }
+  }
+
+  res.json(data)
 })
 
 app.listen(8080, function () {
